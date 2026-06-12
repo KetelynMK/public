69f91aed9afdad491f557e02805dcf8e7f29e0d9
Ketelyn <assiskety846@gmail.com>
2026-06-12 16:26:40 -0300

    d

--- script.js
+++ script.js
@@ -286,3 +286,21 @@ function ajustarHeader() {
 window.addEventListener("resize", ajustarHeader);
 ajustarHeader(); // chama ao carregar
 
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      1024: { slidesPerView: 3 },
+      768: { slidesPerView: 2 },
+      480: { slidesPerView: 1 }
+    }
+  });
